50a6e0ee9105b1ea9c6050aa734e43061337038f
Scott Williams <scottcolemanwilliams@gmail.com>
2019-06-27 13:07:11 -0500

    added contacts, colors, styles

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import './Contact.css'
+
+class Contact extends Component {
+  
+  render() {
+    return (
+      <section className='section contact' id='contact'>
+        <div className='contact-header  section-header'>
+          <h2>Contact me</h2>
+          <p>I'd love to hear from you</p>
+        </div>
+        <div className='line' />
+        <div className='contact-content'>
+          <a href='https://www.linkedin.com/in/scott-williams-8334b2189/'>Linkedin</a>
+          <br/>
+          <a href='https://github.com/ollk'>Github</a>
+          <br/>
+          <a href='mailto:scottcolemanwilliams@gmail.com'>scottcolemanwilliams@gmail.com</a>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Contact
